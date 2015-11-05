359d106a3c02e935a4a354679d320baecc8a7ec4
Scott Weston <scott@ballarathackerspace.org.au>
2015-11-05 17:50:46 +1100

    time -> etime (time is mapped to an import)

--- alarmbot.py
+++ alarmbot.py
@@ -33,7 +33,7 @@ while True:
   result = select.select([s],[],[])
   msg = result[0][0].recv(bufferSize)
   logging.info("recv:%s" % msg)
-  [time, data] = msg.split(' ', 1)
+  [etime, data] = msg.split(' ', 1)
   [channel, argument] = data.split('=')
 
   if not testing:
@@ -42,22 +42,22 @@ while True:
         attachments='[ { "color": "#ffaa00", "fallback": "Alarm status", "title": "Alarm Status", "text": "%s" } ]' % (argument))
 
     if channel == "ballarathackerspace.org.au/motion":
-      warmCache = urllib2.urlopen("https://ballarathackerspace.org.au/webcam%s.jpg" % time).read()
+      warmCache = urllib2.urlopen("https://ballarathackerspace.org.au/webcam%s.jpg" % etime).read()
       if time.time() > next_motion_alert_allowed:
         logging.info("sending motion alert to slack")
         slack.chat.post_message(to_channel, '', username=my_name, icon_emoji=':rotating_light:',
-          attachments='[ { "color": "#ff0000", "fallback": "Motion detected", "title": "Motion Detected", "title_link": "http://axis205.ballarathackerspace.org.au", "image_url": "https://ballarathackerspace.org.au/webcam%s.jpg", "text": "PIR sensor has been tripped %s time(s)." } ]' % (time, argument))
+          attachments='[ { "color": "#ff0000", "fallback": "Motion detected", "title": "Motion Detected", "title_link": "http://axis205.ballarathackerspace.org.au", "image_url": "https://ballarathackerspace.org.au/webcam%s.jpg", "text": "PIR sensor has been tripped %s time(s)." } ]' % (etime, argument))
       next_motion_alert_allowed = time.time() + squelch
 
     if channel == "ballarathackerspace.org.au/light":
       argument = int(argument)
-      warmCache = urllib2.urlopen("https://ballarathackerspace.org.au/webcam%s.jpg" % time).read()
+      warmCache = urllib2.urlopen("https://ballarathackerspace.org.au/webcam%s.jpg" % etime).read()
       if argument > 1500:
         if not lights_on:
           lights_on = True
           logging.info("sending lights detected alert to slack")
           slack.chat.post_message(to_channel, '', username=my_name, icon_emoji=':rotating_light:',
-            attachments='[ { "color": "#00ff00", "fallback": "Lights detected", "title": "Lights Detected", "title_link": "http://axis205.ballarathackerspace.org.au", "image_url": "https://ballarathackerspace.org.au/webcam%s.jpg", "text": "LDR sensor has detected bright light (%s)." } ]' % (time, argument))
+            attachments='[ { "color": "#00ff00", "fallback": "Lights detected", "title": "Lights Detected", "title_link": "http://axis205.ballarathackerspace.org.au", "image_url": "https://ballarathackerspace.org.au/webcam%s.jpg", "text": "LDR sensor has detected bright light (%s)." } ]' % (etime, argument))
       if argument < 1000:
         if lights_on:
           lights_on = False
